ed7748971ae93ce20f43f11e72d8aefe718a3bdd
difelk <dilshan.f@theavo.net>
2024-06-15 09:47:28 +0530

    created common styles and layout for forms V1.1

--- app/src/main/java/com/company/dipizza/view/ui/MainActivity.java
+++ app/src/main/java/com/company/dipizza/view/ui/MainActivity.java
@@ -1,9 +1,10 @@
 package com.company.dipizza.view.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import com.company.dipizza.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
