79d06bb0a60e4672007b9156334a6088c7d3aa25
difelk <dilshan.f@theavo.net>
2024-06-16 18:13:58 +0530

    footer navigation V1.1

---
+++ app/src/main/java/com/company/dipizza/view/ui/common/FooterNavigationView.java
@@ -0,0 +1,15 @@
+package com.company.dipizza.view.ui.common;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.company.dipizza.R;
+
+public class FooterNavigationView extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_footer_navigation);
+    }
+}
